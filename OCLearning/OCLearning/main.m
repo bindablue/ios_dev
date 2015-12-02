4a97fa4d7feadb64e149d78edfd2dc82f5e23dd7
LANDB <landb@LANDBdeMacBook-Pro.local>
2015-12-02 22:25:29 +0800

    First Commit！首次提及Git

---
+++ OCLearning/OCLearning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCLearning
+//
+//  Created by LANDB on 15/12/2.
+//  Copyright (c) 2015年 LANDB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        //怎么开发呢？
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
